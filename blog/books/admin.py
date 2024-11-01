4449ef46876f578117b84a01f702ba34967d5462
Nahian Nazeeb <106859103+NAHIAN-19@users.noreply.github.com>
2024-11-01 11:11:00 +0600

    Delete blog directory

--- blog/books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book, Author, Publisher
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'publisher', 'publication_date', 'price']
-    list_filter = ['author', 'publisher', 'publication_date']
-    search_fields = ['title', 'author', 'publisher', 'publication_date']
-
-admin.site.register(Book, BookAdmin)
